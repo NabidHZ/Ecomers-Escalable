d00798ef070a0182350bb4b479f7bc98c5ae2201
Nabid34_ <nabidhassan34@gmail.com>
2025-06-24 21:05:51 +0200

    Creacion del proyecto

---
+++ Ecomers/src/main/java/com/ecomers/EcomersApplication.java
@@ -0,0 +1,13 @@
+package com.ecomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomersApplication.class, args);
+    }
+
+}
