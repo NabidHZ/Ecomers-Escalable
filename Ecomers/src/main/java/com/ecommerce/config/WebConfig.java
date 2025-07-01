0a9d443565f2092ed7341c3368ad14db0e1a5996
Nabid34_ <nabidhassan34@gmail.com>
2025-07-01 20:43:44 +0200

    CORS configurada para REACT

---
+++ Ecomers/src/main/java/com/ecommerce/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas las rutas
+                        .allowedOrigins("http://localhost:5173") // Permite solicitudes desde el frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permite todos los encabezados
+                        .allowCredentials(true); // Permite el uso de credenciales
+            }
+        };
+    }
+}
